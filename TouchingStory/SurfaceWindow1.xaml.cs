ee494c4370dd6cd4ec48867c4fd776f46439068d
namgiang <namgiang0606@gmail.com>
2014-11-30 10:29:55 +0100

    2nd commit

---
+++ TouchingStory/SurfaceWindow1.xaml.cs
@@ -0,0 +1,161 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Surface;
+using Microsoft.Surface.Presentation;
+using Microsoft.Surface.Presentation.Controls;
+using Microsoft.Surface.Presentation.Input;
+
+namespace TouchingStory
+{
+    /// <summary>
+    /// Interaction logic for SurfaceWindow1.xaml
+    /// </summary>
+    public partial class SurfaceWindow1 : SurfaceWindow
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public SurfaceWindow1()
+        {
+            InitializeComponent();
+            InitializeDefinitions();
+
+            // Add handlers for window availability events
+            AddWindowAvailabilityHandlers();
+        }
+
+        /// <summary>
+        /// Occurs when the window is about to close. 
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            // Remove handlers for window availability events
+            RemoveWindowAvailabilityHandlers();
+        }
+
+        /// <summary>
+        /// Adds handlers for window availability events.
+        /// </summary>
+        private void AddWindowAvailabilityHandlers()
+        {
+            // Subscribe to surface window availability events
+            ApplicationServices.WindowInteractive += OnWindowInteractive;
+            ApplicationServices.WindowNoninteractive += OnWindowNoninteractive;
+            ApplicationServices.WindowUnavailable += OnWindowUnavailable;
+        }
+
+        /// <summary>
+        /// Removes handlers for window availability events.
+        /// </summary>
+        private void RemoveWindowAvailabilityHandlers()
+        {
+            // Unsubscribe from surface window availability events
+            ApplicationServices.WindowInteractive -= OnWindowInteractive;
+            ApplicationServices.WindowNoninteractive -= OnWindowNoninteractive;
+            ApplicationServices.WindowUnavailable -= OnWindowUnavailable;
+        }
+
+        /// <summary>
+        /// This is called when the user can interact with the application's window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnWindowInteractive(object sender, EventArgs e)
+        {
+            //TODO: enable audio, animations here
+        }
+
+        /// <summary>
+        /// This is called when the user can see but not interact with the application's window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnWindowNoninteractive(object sender, EventArgs e)
+        {
+            //TODO: Disable audio here if it is enabled
+
+            //TODO: optionally enable animations here
+        }
+
+        /// <summary>
+        /// This is called when the application's window is not visible or interactive.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnWindowUnavailable(object sender, EventArgs e)
+        {
+            //TODO: disable audio, animations here
+        }
+
+
+        private void InitializeDefinitions()
+        {
+            for (byte k = 1; k <= 5; k++)
+            {
+                TagVisualizationDefinition tagDef =
+                    new TagVisualizationDefinition();
+                // The tag value that this definition will respond to.
+                tagDef.Value = k;
+                // The .xaml file for the UI
+                tagDef.Source =
+                    new Uri("TagKeyword.xaml", UriKind.Relative);
+                // The maximum number for this tag value.
+                tagDef.MaxCount = 2;
+                // The visualization stays for 2 seconds.
+                tagDef.LostTagTimeout = 2000.0;
+                // Orientation offset (default).
+                tagDef.OrientationOffsetFromTag = 0.0;
+                // Physical offset (horizontal inches, vertical inches).
+                tagDef.PhysicalCenterOffsetFromTag = new Vector(2.0, 2.0);
+                // Tag removal behavior (default).
+                tagDef.TagRemovedBehavior = TagRemovedBehavior.Fade;
+                // Orient UI to tag? (default).
+                tagDef.UsesTagOrientation = true;
+                // Add the definition to the collection.
+                MyTagVisualizer.Definitions.Add(tagDef);
+            }
+        }
+
+        private void OnVisualizationAdded(object sender, TagVisualizerEventArgs e)
+        {
+            TagKeyword camera = (TagKeyword)e.TagVisualization;
+            switch (camera.VisualizedTag.Value)
+            {
+                case 1:
+                    camera.CameraModel.Content = "Fabrikam, Inc. ABC-12";
+                    camera.myEllipse.Fill = SurfaceColors.Accent1Brush;
+                    break;
+                case 2:
+                    camera.CameraModel.Content = "Fabrikam, Inc. DEF-34";
+                    camera.myEllipse.Fill = SurfaceColors.Accent2Brush;
+                    break;
+                case 3:
+                    camera.CameraModel.Content = "Fabrikam, Inc. GHI-56";
+                    camera.myEllipse.Fill = SurfaceColors.Accent3Brush;
+                    break;
+                case 4:
+                    camera.CameraModel.Content = "Fabrikam, Inc. JKL-78";
+                    camera.myEllipse.Fill = SurfaceColors.Accent4Brush;
+                    break;
+                default:
+                    camera.CameraModel.Content = "UNKNOWN MODEL";
+                    camera.myEllipse.Fill = SurfaceColors.ControlAccentBrush;
+                    break;
+            }
+        }
+    }
+}
---
+++ TouchingStory/TagKeyword.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Surface;
+using Microsoft.Surface.Presentation;
+using Microsoft.Surface.Presentation.Controls;
+using Microsoft.Surface.Presentation.Input;
+
+namespace TouchingStory
+{
+    /// <summary>
+    /// Interaction logic for TagKeyword.xaml
+    /// </summary>
+    public partial class TagKeyword : TagVisualization
+    {
+        public TagKeyword()
+        {
+            InitializeComponent();
+        }
+
+        private void TagKeyword_Loaded(object sender, RoutedEventArgs e)
+        {
+            //TODO: customize TagKeyword's UI based on this.VisualizedTag here
+        }
+    }
+}
